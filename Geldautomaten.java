33e7b9d123f6d7b2fc329414c8707e540f572ef2
YoshimitsuJ <YoshimitsuJ@users.noreply.github.com>
2021-03-18 23:55:06 +0100

    Update Geldautomaten.java

--- Geldautomaten.java
+++ Geldautomaten.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+import javax.swing.text.StyledEditorKit;
 public class ATM_Transaction
 {
     public static void main(String args[] )
@@ -15,7 +17,38 @@ public class ATM_Transaction
             System.out.println("Choose the operation you want to perform");
             int n = s.nextInt();
             switch(n)
-            break
+            {
+                case 1:
+                System.out.print("Enter money to be withdrawn:");
+                withdraw = s.nextInt();
+                if(balance >= withdraw)
+                {
+                    balance = balance - withdraw;
+                    System.out.println("Please collect your money");
+                }
+                else
+                {
+                    System.out.println("Insufficent Balance");
+                }
+                System.out.println("");
+                break;
+
+                case 2:
+                System.out.print("Enter money to be deposited:");
+                deposit = s.nextInt();
+                balance = balance + deposit;
+                System.out.println("Your Money has been successfully depsited");
+                System.out.println("");
+                break;
+
+                case 3:
+                System.out.println("Balance :"+balance);
+                System.out.println("");
+                break;
+
+                case 4:
+                System.exit(0);
+            }
         }
     }
 }
