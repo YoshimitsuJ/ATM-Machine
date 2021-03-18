3d04c2c6a24e994b2e8b781237a3143928dc9a86
YoshimitsuJ <YoshimitsuJ@users.noreply.github.com>
2021-03-18 23:40:08 +0100

    Create Geldautomaten.java

---
+++ Geldautomaten.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ATM_Transaction
+{
+    public static void main(String args[] )
+    {
+        int balance = 5000, withdraw, deposit;
+        Scanner s = new Scanner (System.in);
+        while(true)
+        {
+            System.out.println("Automated Teller Machine");
+            System.out.println("Choose 1 for Withdraw");
+            System.out.println("Choose 2 for Deposit");
+            System.out.println("Choose 3 for Check Balance");
+            System.out.println("Choose 4 for Exit");
+            System.out.println("Choose the operation you want to perform");
+            int n = s.nextInt();
+            switch(n)
+            break
+        }
+    }
+}
