3b43cf3bbb2f659153e9920eaf7479ae7c65d38a
YoshimitsuJ <YoshimitsuJ@users.noreply.github.com>
2021-03-19 00:04:49 +0100

    Update Geldautomaten.java

--- Geldautomaten.java
+++ Geldautomaten.java
@@ -10,11 +10,11 @@ public class ATM_Transaction
         while(true)
         {
             System.out.println("Automated Teller Machine");
-            System.out.println("Choose 1 for Withdraw");
-            System.out.println("Choose 2 for Deposit");
-            System.out.println("Choose 3 for Check Balance");
-            System.out.println("Choose 4 for Exit");
-            System.out.println("Choose the operation you want to perform");
+            System.out.println("Choose number 1 for Withdraw");
+            System.out.println("Choose number 2 for Deposit");
+            System.out.println("Choose number 3 for Check Balance");
+            System.out.println("Exit");
+            System.out.println("Choose the operation you want to operate");
             int n = s.nextInt();
             switch(n)
             {
@@ -24,11 +24,11 @@ public class ATM_Transaction
                 if(balance >= withdraw)
                 {
                     balance = balance - withdraw;
-                    System.out.println("Please collect your money");
+                    System.out.println("Please save your money");
                 }
                 else
                 {
-                    System.out.println("Insufficent Balance");
+                    System.out.println("Not enough money saved");
                 }
                 System.out.println("");
                 break;
